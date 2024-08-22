dda529d72146bc826a9939e31bcaa0acbc3b74b6
Aleeza <malikaleeza886@gmail.com>
2024-08-22 11:55:56 -0800

    feat:MongoDB func

--- Backend/MongoDB/index.js
+++ Backend/MongoDB/index.js
@@ -24,51 +24,106 @@ const User = mongoose.model("User", userSchema)
 
 
 //GET ALL DOCUMENTS
-User.find({age : 20}).then((res) => {
-    console.log(res[0].name);
-}).catch((err) => {
-    console.log(err);
-})
+// User.find({age : 20}).then((res) => {
+//     console.log(res[0].name);
+// }).catch((err) => {
+//     console.log(err);
+// })
 
 
 //FIND ONE DOCUMENTS
-User.find({age : 20}).then((res) => {
-    console.log(res);
-}).catch((err) => {
-    console.log(err);
-})
+// User.find({age : 20}).then((res) => {
+//     console.log(res);
+// }).catch((err) => {
+//     console.log(err);
+// })
 
 
 //FIND BY ID
-User.findById("66c4e9a95eb238098658bcc5").then((res) => {
+// User.findById("66c4e9a95eb238098658bcc5").then((res) => {
+//     console.log(res);
+// }).catch((err) => {
+//     console.log(err);
+// })
+
+
+//UPDATE ONE DOCUMENT
+// User.updateOne({name : "arooj"}, {email : "arooj@gmail.com"}).then((res) => {
+//     console.log(res);
+// }).catch((err) => {
+//     console.log(err);
+// })
+
+
+//UPDATE MANY DOCUMENTS
+// User.updateMany({age : {$eq : 20}}, {age : 35}).then((res) => {
+//     console.log(res);
+// }).catch((err) => {
+//     console.log(err);
+// })
+
+
+//FIND ONE AND UPDATE THAT FIND DOCUMENT
+// User.findOneAndUpdate({name : "yasir"}, {name : "munir"}, {new : true}).then((res) => {
+//     console.log(res);
+// }).catch((err) => {
+//     console.log(err);
+// })
+
+
+//DELETE ONE DOCUMENT
+// User.deleteOne({name : "munir"}).then((res) => {
+//     console.log(res);
+// }).catch((err) => {
+//     console.log(err);
+// })
+
+
+//DELETE MANY 
+// User.deleteMany({name : "aleeza"}).then((res) => {
+//     console.log(res);
+// }).catch((err) => {
+//     console.log(err);
+// })
+
+
+//FIND BY ID AND DELETE
+// User.findByIdAndDelete("66c4eb7f12e651f21c0fa06a").then((res) => {
+//     console.log(res);
+// }).catch((err) => {
+//     console.log(err);
+// })
+
+
+//FIND ONE AND DELETE
+User.findOneAndDelete({name : "Aleeza"}).then((res) => {
     console.log(res);
 }).catch((err) => {
     console.log(err);
 })
 
-
 //INSERT MANY DOCUMENTS
-User.insertMany([
-    { name: "aleeza", username: "aleeza_78", email: "rubab65@gmail.com", age: "20" },
-    { name: "rubab", username: "ruabab", email: "rubab65@gmail.com", age: "20" },
-    { name: "arooj", username: "arooj_87", email: "rubab65@gmail.com", age: "20" },
-]).then((res) => {
-    console.log(res);
-})
+// User.insertMany([
+//     { name: "aleeza", username: "aleeza_78", email: "rubab65@gmail.com", age: "20" },
+//     { name: "rubab", username: "ruabab", email: "rubab65@gmail.com", age: "20" },
+//     { name: "arooj", username: "arooj_87", email: "rubab65@gmail.com", age: "20" },
+// ]).then((res) => {
+//     console.log(res);
+// })
 
 
 //CREATE AN OBJECT OF MODEL CLASS AND INSERT A SINGLE DOCUMENTS IN USER COLLECTION
-const user2 = new User({
-    name: "rubab",
-    username: "rubab_98",
-    email: "rubab65@gmail.com",
-    age: 20
-});
+// const user2 = new User({
+//     name: "rubab",
+//     username: "rubab_98",
+//     email: "rubab65@gmail.com",
+//     age: 20
+// });
 
 
 //TO SAVE SINGLE DOCUMENT IN DB
-user2.save().then((res) => {
-    console.log(res);
-}).catch((err) => {
-    console.log(err);
-})
+// user2.save().then((res) => {
+//     console.log(res);
+// }).catch((err) => {
+//     console.log(err);
+// })
--- Backend/SQL/index.js
+++ Backend/SQL/index.js
@@ -5,7 +5,7 @@ const mysql = require('mysql2');
 const { faker } = require('@faker-js/faker');
 const methodOverride = require('method-override')
 const { uuid } = require('uuidv4');
-const { log } = require("console");
+
 
 //SET VIEWS
 app.set("view engine", "ejs")
@@ -22,27 +22,9 @@ const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     database: 'record',
-    // password: 'Your SQL password here',
+    password: 'aleezaSQL@12',
 });
 
-//INSERTING DATA WITH PLACEHOLDER TO PASS VALUES DYNAMICALLY
-// let q = "INSERT INTO user (id, username, email, password) VALUES ?";
-// let users = [
-//     ["01b", "aleeza_02", "aleeza45@gmail.com", "ab45@*.._"],
-//     ["01c", "aleeza_03", "aleeza67@gmail.com", "ab667..*)"]
-// ];
-
-// try {
-//     connection.query(q, [users], (err, result) => {
-//         if (err) throw err;
-//         console.log(result);
-
-//     })
-// } catch (err) {
-//     console.log(err);
-// }
-
-// connection.end()
 
 
 //FAKE USERS DATA GENERATOR
@@ -55,28 +37,8 @@ const createRandomUser = () => {
     ]
 }
 
-//ADDING 100 USERS DATA USING FAKER IN MYSQL DB
-// let data = []
-// for(let i=1; i<=100; i++){
-//    data.push(createRandomUser())    
-// }
-
-// //QUERY
-// let q = "INSERT INTO user (id, username, email, password) VALUES ?";
-
-// try {
-//     connection.query(q, [data], (err, result) => {
-//         if (err) throw err;
-//         console.log(result);
-//     })
-// } catch (err) {
-//     console.log(err);
-// }
-
-// connection.end()
 
 //ROUTING
-
 //HOME ROUTE -> GET THE COUNT OF ALL USERS FROM DB
 app.get("/", (req, res) => {
     let q = `SELECT COUNT(*) FROM user`;
@@ -107,6 +69,7 @@ app.get("/user", (req, res) => {
     }
 })
 
+
 //EDIT USER ROUTE -> GET REQUEST TO RENDER EDIT FORM
 app.get("/user/:id/edit", (req, res) => {
     const { id } = req.params
@@ -122,6 +85,7 @@ app.get("/user/:id/edit", (req, res) => {
         res.send("Error in DB.")
     }
 })
+
 
 //UPDATE USER -> PATCH  REQUEST TO UPDATE USER IN DB
 app.patch("/user/:id", (req, res) => {
@@ -203,7 +167,7 @@ app.delete("/user/:id", (req, res) => {
         connection.query(q, (err, result) => {
             if (err) throw err;
             let user = result[0]
-            if (user.email != email && user.password != password) {
+            if (user.email !== email || user.password !== password) {
                 res.render("deleteUser.ejs", {user, errorMessage : "Incorrect email or password."})
             } else {
                 // QUERY FOR DELETE
