cd39384de68ad916486875061e607b3c26910554
Aleeza Rubab <malikaleeza886@gmail.com>
2024-10-03 09:56:43 -0800

    feat: reviews

--- Airbnb/models/listing.js
+++ Airbnb/models/listing.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const Review = require("./reviews.js")
 
 //define schema
 const listingSchema = new Schema({
@@ -19,6 +20,13 @@ const listingSchema = new Schema({
             ref: "Review"
         }
     ]
+})
+
+//post middleware to delete reviews from review model when delete a list
+listingSchema.post("findOneAndDelete", async (listing) => {
+    if (listing) {
+        await Review.deleteMany({ _id: { $in: listing.reviews } })
+    }
 })
 
 //create collection
--- Airbnb/public/js/script.js
+++ Airbnb/public/js/script.js
@@ -17,3 +17,37 @@
       }, false)
     })
   })()
+
+
+  document.addEventListener('DOMContentLoaded', function () {
+    const stars = document.querySelectorAll('.star-rating .fa-star');
+    const ratingInput = document.getElementById('rating');
+
+    stars.forEach(star => {
+        star.addEventListener('click', function () {
+            const value = this.getAttribute('data-value');
+            ratingInput.value = value;
+            highlightStars(value);
+        });
+
+        star.addEventListener('mouseover', function () {
+            const value = this.getAttribute('data-value');
+            highlightStars(value);
+        });
+
+        star.addEventListener('mouseout', function () {
+            const value = ratingInput.value;
+            highlightStars(value);
+        });
+    });
+
+    function highlightStars(value) {
+        stars.forEach(star => {
+            if (star.getAttribute('data-value') <= value) {
+                star.classList.add('selected');
+            } else {
+                star.classList.remove('selected');
+            }
+        });
+    }
+});
